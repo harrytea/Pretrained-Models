bbcaf384ec7a880c21b127a9952c79f0e2039390
harrytea <1061771439@qq.com>
2022-04-27 11:03:31 +0800

    first update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Pretrained-Models
-Pretrained-Models
+
+- [News](#news)
+- [Vision Transformer](#VisionTransformer)
+
+## News
+
+:smile: **Continue update**
+
+**2022.04.27**
+
+* update vision transformer
+
+## VisionTransformer
+
+You can download the pretrained model from [ViT](https://github.com/rwightman/pytorch-image-models/blob/f55c22bebf9d8afc449d317a723231ef72e0d662/timm/models/vision_transformer.py#L54-L106). Another link is [here](https://console.cloud.google.com/storage/browser/vit_models;tab=objects?prefix=&forceOnObjectsSortingFiltering=false&pageState=(%22StorageObjectListTable%22:(%22f%22:%22%255B%255D%22)))
+
+The official pretrained vit model is `npz` format. You can use `VisionTransformer/jax_pop.py` or  `VisionTransformer/jax_convert.py` to convert it into `pth` file.
+
+```python
+python jax_convert.py --src 'xxx.npz' --dst 'xxx.pth'
+```
+
+after converting, you can create corresponding model from `VisionTransformer/vit.py` and load the weight. All vit models can be refered to [vision transformer](https://github.com/rwightman/pytorch-image-models/blob/f55c22bebf9d8afc449d317a723231ef72e0d662/timm/models/vision_transformer.py)
---
+++ VisionTransformer/vit.py
@@ -0,0 +1,162 @@
+"""
+Adapted from 2020 Ross Wightman
+https://github.com/rwightman/pytorch-image-models
+"""
+import math
+import torch
+import torch.nn as nn
+from blocks import Block, PatchEmbedding
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        trunc_normal_(m.weight, std=0.02)
+        if isinstance(m, nn.Linear) and m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.LayerNorm):
+        nn.init.constant_(m.bias, 0)
+        nn.init.constant_(m.weight, 1.0)
+
+
+def resize_pos_embed(posemb, grid_old_shape, grid_new_shape, num_extra_tokens):
+    # Rescale the grid of position embeddings when loading from state_dict. Adapted from
+    # https://github.com/google-research/vision_transformer/blob/00883dd691c63a6830751563748663526e811cee/vit_jax/checkpoint.py#L224
+    posemb_tok, posemb_grid = (
+        posemb[:, :num_extra_tokens],
+        posemb[0, num_extra_tokens:],
+    )
+    if grid_old_shape is None:
+        gs_old_h = int(math.sqrt(len(posemb_grid)))
+        gs_old_w = gs_old_h
+    else:
+        gs_old_h, gs_old_w = grid_old_shape
+
+    gs_h, gs_w = grid_new_shape
+    posemb_grid = posemb_grid.reshape(1, gs_old_h, gs_old_w, -1).permute(0, 3, 1, 2)
+    posemb_grid = F.interpolate(posemb_grid, size=(gs_h, gs_w), mode="bilinear")
+    posemb_grid = posemb_grid.permute(0, 2, 3, 1).reshape(1, gs_h * gs_w, -1)
+    posemb = torch.cat([posemb_tok, posemb_grid], dim=1)
+    return posemb
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, image_size, patch_size, n_layers, d_model, d_ff, n_heads, \
+        n_cls, dropout=0.1, drop_path_rate=0.0, distilled=False, channels=3,):
+        super().__init__()
+        self.patch_embed = PatchEmbedding(image_size, patch_size, d_model, channels,)
+        self.patch_size = patch_size
+        self.n_layers = n_layers
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.n_heads = n_heads
+        self.dropout = nn.Dropout(dropout)
+        self.n_cls = n_cls
+
+        # cls and pos tokens
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, d_model))
+        self.distilled = distilled
+        if self.distilled:
+            self.dist_token = nn.Parameter(torch.zeros(1, 1, d_model))
+            self.pos_embed = nn.Parameter(torch.randn(1, self.patch_embed.num_patches + 2, d_model))
+            self.head_dist = nn.Linear(d_model, n_cls)
+        else:
+            self.pos_embed = nn.Parameter(torch.randn(1, self.patch_embed.num_patches + 1, d_model))
+
+        # transformer blocks
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, n_layers)]
+        self.layers = nn.ModuleList([Block(d_model, n_heads, d_ff, dropout, dpr[i]) for i in range(n_layers)])
+
+        # output head
+        self.norm = nn.LayerNorm(d_model)
+        self.head = nn.Linear(d_model, n_cls)
+
+        trunc_normal_(self.pos_embed, std=0.02)
+        trunc_normal_(self.cls_token, std=0.02)
+        if self.distilled:
+            trunc_normal_(self.dist_token, std=0.02)
+        self.pre_logits = nn.Identity()
+
+        self.apply(init_weights)
+
+
+    def forward(self, im, return_features=False):
+        B, _, H, W = im.shape
+        PS = self.patch_size
+
+        x = self.patch_embed(im)
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        if self.distilled:
+            dist_tokens = self.dist_token.expand(B, -1, -1)
+            x = torch.cat((cls_tokens, dist_tokens, x), dim=1)
+        else:
+            x = torch.cat((cls_tokens, x), dim=1)
+
+        pos_embed = self.pos_embed
+        num_extra_tokens = 1 + self.distilled
+        if x.shape[1] != pos_embed.shape[1]:
+            pos_embed = resize_pos_embed(
+                pos_embed,
+                self.patch_embed.grid_size,
+                (H // PS, W // PS),
+                num_extra_tokens,
+            )
+        x = x + pos_embed
+        x = self.dropout(x)
+
+        for blk in self.layers:
+            x = blk(x)
+        x = self.norm(x)
+
+        if return_features:
+            return x
+
+        if self.distilled:
+            x, x_dist = x[:, 0], x[:, 1]
+            x = self.head(x)
+            x_dist = self.head_dist(x_dist)
+            x = (x + x_dist) / 2
+        else:
+            x = x[:, 0]
+            x = self.head(x)
+        return x
+
+    def get_attention_map(self, im, layer_id):
+        if layer_id >= self.n_layers or layer_id < 0:
+            raise ValueError(
+                f"Provided layer_id: {layer_id} is not valid. 0 <= {layer_id} < {self.n_layers}."
+            )
+        B, _, H, W = im.shape
+        PS = self.patch_size
+
+        x = self.patch_embed(im)
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        if self.distilled:
+            dist_tokens = self.dist_token.expand(B, -1, -1)
+            x = torch.cat((cls_tokens, dist_tokens, x), dim=1)
+        else:
+            x = torch.cat((cls_tokens, x), dim=1)
+
+        pos_embed = self.pos_embed
+        num_extra_tokens = 1 + self.distilled
+        if x.shape[1] != pos_embed.shape[1]:
+            pos_embed = resize_pos_embed(
+                pos_embed,
+                self.patch_embed.grid_size,
+                (H // PS, W // PS),
+                num_extra_tokens,
+            )
+        x = x + pos_embed
+
+        for i, blk in enumerate(self.blocks):
+            if i < layer_id:
+                x = blk(x)
+            else:
+                return blk(x, return_attention=True)
+
+
+if __name__=='__main__':
+    model = VisionTransformer(image_size=(384,384), patch_size=16, n_layers=12, d_model=192, d_ff=768, n_heads=3, \
+        n_cls=1000, dropout=0.1, drop_path_rate=0.0, distilled=False, channels=3,)
+    checkpoint = torch.load('vit_tiny_p16_384.pth')
+    model.load_state_dict(checkpoint)
